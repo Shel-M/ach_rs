0e2e0006d3bf86ab523ea5b06faf6febca6f9c05
SheldonM <blackshadow606@yahoo.com>
2021-11-09 17:36:13 -0700

    Added more Record structs.

--- src/ach_lib_rs/src/lib.rs
+++ src/ach_lib_rs/src/lib.rs
@@ -1,37 +1,74 @@
-// https://dev-ach-guide.pantheonsite.io/ach-file-details
-// Nacha ACH file implemented in Rust
+// https://achdevguide.nacha.org/ach-file-details
 
-trait AchRecord{ }
-
-struct AchFile {
-    header: AchHeader,
-    records: Vec<Box<dyn AchRecord>>,
-
+trait Record {
+	const REC_TYPE_CODE: Field;
 }
 
-struct AchField {
-    field_num: u8,
-    content: String,
-    size: u32,
+struct File {
+	header: Header,
+	records: Vec<Box<dyn Record>>,
+	
 }
 
-struct AchHeader {
-    rec_type_code: AchField,        // content: "1", size: 1
-    priority_code: AchField,        // content:  "01", size: 2
-    immediate_dest: AchField,       // size: 10
-    immediate_orig: AchField,       // size: 10
-    file_creation_date: AchField,   // size: 6
-    file_creation_time: AchField,   // size: 4
-    file_id_modifier: AchField,     // size: 1
-    record_size: AchField,          // content: "094", size: 3
-    blocking_factor: AchField,      // content: "10", size: 2
-    format_code: AchField,          // content: "1", size: 1
-    immediate_dest_name: AchField,  // size: 23
-    immediate_orig_name: AchField,  // size: 23
-    reference_code: AchField,       // content: "", size: 8
+struct Field {
+	content: String,
+	size: u32,
 }
 
-impl AchHeader { }
+impl Field {
+	fn new(content: &str) -> Self{
+		Field {
+			content: content.to_string(),
+			size: 1
+		}
+	}
+}
 
-impl AchRecord for AchHeader { }
+struct Header {
+	priority_code: Field,       // content:  "01", size: 2
+	immediate_dest: Field,      // size: 10
+	immediate_orig: Field,      // size: 10
+	file_creation_date: Field,  // size: 6
+	file_creation_time: Field,  // size: 4
+	file_id_modifier: Field,    // size: 1
+	record_size: Field,         // content: "094", size: 3
+	blocking_factor: Field,     // content: "10", size: 2
+	format_code: Field,         // content: "1", size: 1
+	immediate_dest_name: Field, // size: 23
+	immediate_orig_name: Field, // size: 23
+	reference_code: Field,      // content: "", size: 8
+}
 
+impl Record for Header {
+	const REC_TYPE_CODE: Field = Field::new("1");
+}
+
+struct CompanyBatchHeader {
+	service_class_code: Field,          // size: 3
+	company_name: Field,                // size: 16
+	company_discretionary_data: Field,  // size: 20
+	company_id: Field,                  // size: 10
+	sec: Field,                         // size: 3
+	entry_desc: Field,                  // size: 10
+	company_descriptive_date: Field,    // size: 6
+	effective_entry_date: Field,        // size: 6
+	settlement_date: Field,             // size: 3
+	originator_status_code: Field,      // size: 1
+	odfi_id: Field,                     // size: 8
+	batch_number: Field,                // size: 7
+}
+
+impl Record for CompanyBatchHeader {
+	const REC_TYPE_CODE: Field = Field::new("5");
+}
+
+struct EntryDetail {
+	transactions_code: Field,   // size: 2
+	receiving_dfi_id: Field,    // size: 8
+	check_digit: Field,         // size: 1
+	dfi_account
+}
+
+impl Record for EntryDetail {
+	const REC_TYPE_CODE: Field = Field::new("6");
+}
