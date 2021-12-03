470f986faf7cc33746769194aa2413ca827c57c0
SheldonM <blackshadow606@yahoo.com>
2021-12-03 15:55:01 -0700

    Early testing of public AchFile functions and construction.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ach_lib_rs::ach_file::AchFile;
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let ach = AchFile::try_from(Path::new(&args[1])).unwrap();
+    print!("{}", ach);
+
+    println!("{}", ach.len())
 }
